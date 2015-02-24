7bcc982812fd202d6b76b25cfcd1276c0d54d2ca
TamerlanImanov <tamerlan.imanov@ce.sdu.edu.kz>
2015-02-24 15:20:31 +0600

    Project

---
+++ MyPharmacy/MyPharmacy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPharmacy
+//
+//  Created by Tamerlan on 24.02.15.
+//  Copyright (c) 2015 Tamerlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
